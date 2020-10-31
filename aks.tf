48fa77f0eb7e408fa3fba71f161f071b1bf48740
Taylor Dolezal <onlydole@gmail.com>
2020-10-30 18:07:08 -0700

    Refactor AKS module setup

--- aks.tf
+++ aks.tf
@@ -3,22 +3,26 @@ resource "azurerm_resource_group" "project" {
   location = var.region
 }
 
-resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = var.project_name
-  location            = azurerm_resource_group.project.location
+module "network" {
+  source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.project.name
-  dns_prefix          = var.project_name
-
-  default_node_pool {
-    name       = "default"
-    node_count = 3
-    vm_size    = "Standard_D2_v2"
+  address_space       = "11.0.0.0/16"
+  subnet_prefixes     = ["11.0.1.0/24"]
+  subnet_names        = ["subnet1"]
+  depends_on          = [azurerm_resource_group.project]
+  tags = {
+    Operator = "Terraform"
   }
+}
 
-  identity {
-    type = "SystemAssigned"
-  }
-
+module "aks" {
+  source              = "Azure/aks/azurerm"
+  resource_group_name = azurerm_resource_group.project.name
+  prefix              = var.project_name
+  agents_count        = 3
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+  os_disk_size_gb     = 100
+  depends_on          = [module.network]
   tags = {
     Operator = "Terraform"
   }
--- helm_datadog.tf
+++ helm_datadog.tf
@@ -1,12 +1,12 @@
 provider "helm" {
   kubernetes {
-    load_config_file       = false
-    host                   = azurerm_kubernetes_cluster.cluster.kube_config.0.host
-    username               = azurerm_kubernetes_cluster.cluster.kube_config.0.username
-    password               = azurerm_kubernetes_cluster.cluster.kube_config.0.password
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+    load_config_file = false
+
+    host = module.aks.host
+
+    client_certificate     = base64decode(module.aks.client_certificate)
+    client_key             = base64decode(module.aks.client_key)
+    cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
   }
 }
 
@@ -14,7 +14,7 @@ resource "helm_release" "datadog_agent" {
   name       = "datadog-agent"
   repository = "https://helm.datadoghq.com"
   chart      = "datadog"
-  version    = "2.4.30"
+  version    = "2.4.31"
 
   set_sensitive {
     name  = "datadog.apiKey"
@@ -26,47 +26,7 @@ resource "helm_release" "datadog_agent" {
     value = var.datadog_app_key
   }
 
-  set {
-    name  = "datadog.logs.enabled"
-    value = true
-  }
-
-  set {
-    name  = "datadog.logs.containerCollectAll"
-    value = true
-  }
-
-  set {
-    name  = "clusterAgent.enabled"
-    value = true
-  }
-
-  set {
-    name  = "clusterAgent.metricsProvider.enabled"
-    value = true
-  }
-
-  set {
-    name  = "datadog.processAgent.enabled"
-    value = true
-  }
-
-  set {
-    name  = "datadog.processAgent.processCollection"
-    value = true
-  }
-  set {
-    name  = "datadog.leaderElection"
-    value = true
-  }
-
-  set {
-    name  = "rbac.create"
-    value = true
-  }
-
-  set {
-    name  = "datadog.collectEvents"
-    value = true
-  }
+  values = [
+    file("datadog.yaml")
+  ]
 }
